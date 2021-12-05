3b5e899c97d38bcf57e54b4188f0630207104802
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2021-12-05 22:24:38 +0000

    getColonyQtlGeno()

--- AlphaSimRBee/R/Functions_L0_auxilary.R
+++ AlphaSimRBee/R/Functions_L0_auxilary.R
@@ -1556,7 +1556,7 @@ getQueensQtlGeno <- function(x,
 getFathersQtlGeno <- function(x, nInd = NULL,
                               trait = 1, chr = NULL, simParam = NULL) {
   ret <- getCasteQtlGeno(x, caste = "fathers", nInd = nInd,
-                         trait = trait, chhr = chr, simParam = simParam)
+                         trait = trait, chr = chr, simParam = simParam)
   return(ret)
 }
 
@@ -1584,6 +1584,112 @@ getDronesQtlGeno <- function(x, nInd = NULL,
                              trait = 1, chr = NULL, simParam = NULL) {
   ret <- getCasteQtlGeno(x, caste = "drones", nInd = nInd,
                          trait = trait, chr = chr, simParam = simParam)
+  return(ret)
+}
+
+#' @rdname getColonyQtlGeno
+#' @title Access QTL genotype data of individuals in colony
+#'
+#' @description Access QTL genotype data of individuals in colony.
+#'
+#' @param x Colony or Colonies
+#' @param caste character, a combination of "queen", "fathers", "virgin_queens",
+#' "workers", or "drones"
+#' @param nInd numeric, number of individuals to access, if NULL all individuals
+#' are accessed, otherwise a random sample; can be a list to access different
+#' number of different caste - when this is the case \code{nInd} takes precedence over
+#' \code{caste} (see examples)
+#' @param trait numeric, indicates which trait's QTL genotype to retrieve
+#' @param chr numeric, chromosomes to retrieve, if NULL, all chromosome are retrieved
+#' @param simParam SimParam
+#'
+#' @details
+#'
+#' @seealso \code{\link{getCasteQtlGeno}} and \code{\link{getQtlGeno}}
+#'
+#' @examples
+#' # AlphaSimR
+#' founderGenomes <- quickHaplo(nInd = 3, nChr = 1, segSites = 10)
+#' SP <- SimParam$new(founderGenomes)
+#' SP$addTraitA(nQtlPerChr = 10)
+#' basePop <- newPop(founderGenomes)
+#'
+#' # Honeybee
+#' drones <- createFounderDrones(pop = basePop[1], nDronesPerQueen = 10)
+#' colony1 <- createColony(queen = basePop[2], fathers = drones[1:5])
+#' colony2 <- createColony(queen = basePop[3], fathers = drones[6:10])
+#' colony1 <- addWorkers(colony1, nInd = 10)
+#' colony2 <- addWorkers(colony2, nInd = 20)
+#' colony1 <- addDrones(colony1, nInd = 2)
+#' colony2 <- addDrones(colony2, nInd = 4)
+#'
+#' getColonyQtlGeno(colony1)
+#' getColonyQtlGeno(colony1, caste = c("queen", "fathers"))
+#' getColonyQtlGeno(colony1, nInd = 1)
+#' getColonyQtlGeno(colony1, nInd = list("queen" = 1, "fathers" = 2, "virgin_queens" = 1))
+#'
+#' apiary <- c(colony1, colony2)
+#' getColonyQtlGeno(apiary)
+#' getColonyQtlGeno(apiary, caste = c("queen", "fathers"))
+#' getColonyQtlGeno(apiary, nInd = 1)
+#' getColonyQtlGeno(apiary, nInd = list("queen" = 1, "fathers" = 2, "virgin_queens" = 1))
+#'
+#' @return list of matrices with haplotypes when \code{x} is Colony (list nodes
+#' named by caste) and list of a list of matrices with haplotypes when \code{x}
+#' is Colonies, outer list is named by colony id when \code{x} is Colonies
+#'
+#' @export
+getColonyQtlGeno <- function(x, caste = c("queen", "fathers", "virgin_queens", "workers", "drones"), nInd = NULL,
+                             trait = 1, chr = NULL, simParam = NULL) {
+  if ("Colony" %in% class(x)) {
+    if (is.list(nInd)) {
+      caste <- names(nInd)
+    } else {
+      if (length(nInd) > 1) {
+        warning("Using only the first value of nInd!")
+      }
+      nIndOrig <- nInd
+      nInd <- vector(mode = "list", length = length(caste))
+      if (!is.null(nIndOrig)) {
+        for (node in 1:length(caste)) {
+          nInd[[node]] <- nIndOrig
+        }
+      }
+      names(nInd) <- caste
+    }
+    ret <- vector(mode = "list", length = length(caste))
+    names(ret) <- caste
+    if ("queen" %in% caste) {
+      ret$queen <- getQueensQtlGeno(x = x,
+                                    trait = trait, chr = chr, simParam = simParam)
+    }
+    if ("fathers" %in% caste) {
+      ret$fathers <- getFathersQtlGeno(x = x, nInd = nInd$fathers,
+                                       trait = trait, chr = chr, simParam = simParam)
+    }
+    if ("virgin_queens" %in% caste) {
+      ret$virgin_queens <- getVirginQueensQtlGeno(x = x, nInd = nInd$virgin_queens,
+                                                  trait = trait, chr = chr, simParam = simParam)
+    }
+    if ("workers" %in% caste) {
+      ret$workers <- getWorkersQtlGeno(x = x, nInd = nInd$workers,
+                                       trait = trait, chr = chr, simParam = simParam)
+    }
+    if ("drones" %in% caste) {
+      ret$drones <- getDronesQtlGeno(x = x, nInd = nInd$drones,
+                                     trait = trait, chr = chr, simParam = simParam)
+    }
+  } else if ("Colonies" %in% class(x)) {
+    nCol <- nColonies(x)
+    ret <- vector(mode = "list", length = nCol)
+    for (colony in 1:nCol) {
+      ret[[colony]] <- getColonyQtlGeno(x = x@colonies[[colony]], caste = caste, nInd = nInd,
+                                        trait = trait, chr = chr, simParam = simParam)
+    }
+    names(ret) <- getId(x)
+  } else {
+    stop("Argument x must be a Colony or Colonies class object!")
+  }
   return(ret)
 }
 
@@ -2108,7 +2214,7 @@ getQueensSnpGeno <- function(x,
 getFathersSnpGeno <- function(x, nInd = NULL,
                               snpChip = 1, chr = NULL, simParam = NULL) {
   ret <- getCasteSnpGeno(x, caste = "fathers", nInd = nInd,
-                         snpChip = snpChip, chhr = chr, simParam = simParam)
+                         snpChip = snpChip, chr = chr, simParam = simParam)
   return(ret)
 }
 
