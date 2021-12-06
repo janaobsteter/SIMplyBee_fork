2cc8681a68457eeaea303278620c1c3a8b44f2d7
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2021-12-06 18:06:03 +0000

    Added also isPop()

--- AlphaSimRBee/R/Classes.R
+++ AlphaSimRBee/R/Classes.R
@@ -1,3 +1,11 @@
+#' @describeIn Pop Test if object is a Pop class object
+# TOOD: I have provided this as PullRequest for AlphaSimR https://github.com/gaynorr/AlphaSimR/pull/31/commits/ecfa286a05f7e2f3f54ec5bc3a741b1786d183c4
+#   once it gets incorporated there we should remove it here
+isPop = function(x) {
+  ret = is(x, class2 = "isPop")
+  return(ret)
+}
+
 # Class Colony ----
 
 #' @rdname Colony
