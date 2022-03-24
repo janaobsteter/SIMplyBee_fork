64b9b314ec3be15ccfbf65774c6602deb496baea
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2022-03-24 15:47:20 +0000

    Polished Seeley ref

--- SIMplyBee/R/Functions_L2_Colony.R
+++ SIMplyBee/R/Functions_L2_Colony.R
@@ -586,11 +586,7 @@ buildUpColony <- function(colony, nWorkers = NULL, nDrones = NULL,
 #'   in preparation for the winter months.
 #'
 #' @param colony \code{\link{Colony-class}}
-#' @param p numeric, percentage of workers to remove from the colony
-#'     @references Seeley, T. (2019).
-#'     The Lives of Bees: The Untold Story of the Honey Bee in the Wild.
-#'      Princeton: Princeton University Press.
-#'      \url{https://doi-org.ezproxy.is.ed.ac.uk/10.1515/9780691189383}
+#' @param p numeric, percentage of workers to remove from the colony (Seeley, 2019)
 #' @param use character, all the options provided by \code{\link{selectInd}};
 #'   it guides the selection of workers that will be removed
 #' @param new logical, should the number of workers and drones be added anew or
@@ -598,6 +594,10 @@ buildUpColony <- function(colony, nWorkers = NULL, nDrones = NULL,
 #'   \code{nWorkers} and \code{nDrones} (see details)
 #'
 #' @return \code{\link{Colony-class}} with workers reduced and drones/virgin queens removed
+#'
+#' @references Seeley (2019) The Lives of Bees: The Untold Story of the Honey
+#'   Bee in the Wild. Princeton: Princeton University Press.
+#'   \url{https://doi-org.ezproxy.is.ed.ac.uk/10.1515/9780691189383}
 #'
 #' @examples
 #' founderGenomes <- quickHaplo(nInd = 2, nChr = 1, segSites = 100)
