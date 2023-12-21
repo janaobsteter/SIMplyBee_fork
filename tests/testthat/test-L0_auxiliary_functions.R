e354bb26cf212138e4b85ac78fb55fcf7d24809f
LStrachan <s2122596.ed.ac.uk>
2023-12-21 10:44:13 +0100

    added getCaste() tests, all working, ran a devtools::check too

--- tests/testthat/test-L0_auxiliary_functions.R
+++ tests/testthat/test-L0_auxiliary_functions.R
@@ -626,8 +626,6 @@ test_that("editCsdLocus", {
 })
 
 # ---- emptyNULL ----
-
-####----- emptyNULL ---- ####
 test_that("emptyNULL", {
   founderGenomes <- quickHaplo(nInd = 5, nChr = 1, segSites = 100)
   SP <- SimParamBee$new(founderGenomes, csdChr = 1, nCsdAlleles = 8)
@@ -899,6 +897,7 @@ test_that("getLocation", {
                 list("2" = loc, "3" = loc))
 })
 
+# ---- createCrossPlan ----
 test_that("createCrossPlan", {
   founderGenomes <- quickHaplo(nInd = 1000, nChr = 1, segSites = 100)
   SP <- SimParamBee$new(founderGenomes)
@@ -953,4 +952,55 @@ test_that("createCrossPlan", {
   expect_length(crossPlanSpatial, 2)
   expect_error(createCrossPlan(x = droneColonies, droneColonies = virginColonies1, simParamBee = SP))
 })
+
+
+# ---- Get Caste ----
+test_that("getCaste", {
+  founderGenomes <- quickHaplo(nInd = 1000, nChr = 1, segSites = 100)
+  SP <- SimParamBee$new(founderGenomes)
+  SP$nThreads = 1L
+  basePop <- createVirginQueens(founderGenomes, simParamBee = SP)
+
+  #Create drones
+  drones <- createDrones(x = basePop[1], nInd = 1000, simParamBee = SP)
+  droneGroups <- pullDroneGroupsFromDCA(drones, n = 10, nDrones = nFathersPoisson, simParamBee = SP)
+
+  # Create a Colony and a MultiColony class
+  colony <- createColony(x = basePop[2], simParamBee = SP)
+  colony <- cross(colony, drones = droneGroups[[1]], simParamBee = SP)
+  colony <- buildUp(x = colony, nWorkers = 20, nDrones = 5, simParamBee = SP)
+  colony <- addVirginQueens(colony, nInd = 5, simParamBee = SP)
+
+  apiary <- createMultiColony(basePop[3:4], n = 2, simParamBee = SP)
+  apiary <- cross(apiary, drones = droneGroups[c(2, 3)], simParamBee = SP)
+  apiary <- buildUp(x = apiary, nWorkers = 10, nDrones = 2, simParamBee = SP)
+  apiary <- addVirginQueens(apiary, nInd = 4, simParamBee = SP)
+
+  expect_vector(getCaste(getQueen(colony, simParamBee = SP), simParamBee = SP), "queen")
+  expect_vector(getCaste(getFathers(colony, simParamBee = SP), simParamBee = SP), "fathers")
+  expect_vector(getCaste(getWorkers(colony, simParamBee = SP), simParamBee = SP), "workers")
+  expect_vector(getCaste(getDrones(colony, simParamBee = SP), simParamBee = SP), "drones")
+  expect_vector(getCaste(getVirginQueens(colony, simParamBee = SP), simParamBee = SP), "virginQueen")
+
+  bees <- c(
+    getQueen(colony, simParamBee = SP),
+    getFathers(colony, nInd = 2, simParamBee = SP),
+    getWorkers(colony, nInd = 2, simParamBee = SP),
+    getDrones(colony, nInd = 2, simParamBee = SP),
+    getVirginQueens(colony, nInd = 2, simParamBee = SP)
+  )
+  expect_vector(getCaste(bees, simParamBee = SP), c("queen", "fathers", "workers", "drones", "virginQueens"))
+
+  expect_length(getCaste(colony, collapse = FALSE, simParamBee = SP), 5) #This is a list
+
+  # Collapse information into a single vector
+  expect_vector(getCaste(colony, collapse = TRUE, simParamBee = SP), c("queen", "fathers", "workers", "drones", "virginQueens"))
+
+  #Apiary contains 2 colonies
+  expect_length(getCaste(apiary, simParamBee = SP), 2)
+})
+
+
+
+
 
