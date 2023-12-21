afbe0d6197202584ce5627c416fd5d21c486c5b2
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2023-12-21 12:51:22 +0100

    Fixed an error in R/SIMplyBee.R - it did not show everything in man file

--- R/SIMplyBee.R
+++ R/SIMplyBee.R
@@ -7,17 +7,17 @@
 # see https://r-pkgs.org/namespace.html on description what to import/depend/...
 
 #' @description
-# SIMplyBee is an extension of the AlphaSimR package
-# (https://cran.r-project.org/package=AlphaSimR) for stochastic simulations of
-# honeybee populations and breeding programmes. It enables simulation of
-# individual bees that form a colony, which includes a queen, fathers (drones
-# the queen mated with), virgin queens, workers, and drones. Multiple colony can
-# be merged into a population of colonies, such as an apiary or a whole country
-# of colonies. Functions enable operations on castes, colony, or colonies, to
-# ease R scripting of whole populations. All AlphaSimR functionality with
-# respect to genomes and genetic and phenotype values is available and further
-# extended for honeybees, including haplo-diploidy, csd locus, colony events
-# (swarming, supersedure, etc.), and colony phenotype values.
+#' SIMplyBee is an extension of the AlphaSimR package
+#' (https://cran.r-project.org/package=AlphaSimR) for stochastic simulations of
+#' honeybee populations and breeding programmes. It enables simulation of
+#' individual bees that form a colony, which includes a queen, fathers (drones
+#' the queen mated with), virgin queens, workers, and drones. Multiple colony can
+#' be merged into a population of colonies, such as an apiary or a whole country
+#' of colonies. Functions enable operations on castes, colony, or colonies, to
+#' ease R scripting of whole populations. All AlphaSimR functionality with
+#' respect to genomes and genetic and phenotype values is available and further
+#' extended for honeybees, including haplo-diploidy, csd locus, colony events
+#' (swarming, supersedure, etc.), and colony phenotype values.
 #'
 #' See the introductory vignette on using this package by running:
 #' \code{vignette("Honeybee_biology", package="SIMplyBee")}
